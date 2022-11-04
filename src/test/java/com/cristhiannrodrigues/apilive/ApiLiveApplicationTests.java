55f00b053d219e68009f71e43bb39e6bdfb32093
Cristhiann Rodrigues <cristhiann.martins@opensingular.com>
2022-11-04 15:40:12 -0300

    first commit

---
+++ src/test/java/com/cristhiannrodrigues/apilive/ApiLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhiannrodrigues.apilive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
